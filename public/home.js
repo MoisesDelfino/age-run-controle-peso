7cdba8af6fb95030c315ebbd3a7182c030e32b3e
Moises Delfino <moisescamposdelfino@gmail.com>
2026-05-16 19:05:28 -0300

    Fix: Corrige API_BASE para funcionar em produção (detecta ambiente automaticamente)

--- public/home.js
+++ public/home.js
@@ -1,5 +1,7 @@
 // Configuração da API
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = window.location.hostname === 'localhost' 
+    ? 'http://localhost:3000/api' 
+    : '/api';
 
 let usuarioLogado = null;
 
